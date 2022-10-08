9b536dbbf6dd5d8a202d6f271a9d7c9420d17dfc
Bruno Peixoto <brunolnetto@gmail.com>
2022-10-08 13:35:03 -0300

    feat/ epoch date

--- utils.js
+++ utils.js
@@ -1,7 +1,20 @@
 /**
+ * @abstract returns ISO date String
+ *
+ * @return {String}
+ */
+export const now_ISO = () => (new Date(Date.now())).toISOString();
+
+/**
  * @abstract returns date String
  *
  * @return {String}
  */
-export const now = () => (new Date(Date.now())).toISOString();
+export const now_date = () => (new Date(Date.now())).toDateString();
  
+/**
+ * @abstract returns milliseconds from epoch date
+ *
+ * @return {String}
+ */
+export const now_epoch = () => (new Date().getTime());
